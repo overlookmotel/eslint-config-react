e957b0a4939980b5ea0d814d296789b7c39be658
overlookmotel <theoverlookmotel@gmail.com>
2020-02-09 19:38:52 +0000

    Remove old commented-out code [nocode]

--- index.js
+++ index.js
@@ -56,24 +56,3 @@ module.exports = {
 		'node/prefer-global/url': ['off']
 	}
 };
-
-/*
-// Exports
-module.exports = {
-	plugins: [
-		'react'
-	],
-	extends: [
-		'plugin:react/recommended'
-	],
-	rules: {
-		'react/prop-types': ['off'],
-		'no-extra-parens': ['error', 'all', {ignoreJSX: 'multi-line'}]
-	},
-	settings: {
-		react: {
-			version: 'detect'
-		}
-	}
-};
-*/
