4e2efe2501817997184fc2cbf2bdcade388bab7a
overlookmotel <theoverlookmotel@gmail.com>
2022-03-24 23:26:05 +0000

    Order rules in alphabetical order [refactor]

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ module.exports = {
 
 	rules: {
 		// Alterations of airbnb rules
-		'react/prop-types': ['off'],
 		'react/destructuring-assignment': ['off'],
+		'react/jsx-filename-extension': ['error', {extensions: ['.jsx', '.test.js']}],
 		'react/jsx-indent': ['error', 'tab', {checkAttributes: true}],
 		'react/jsx-indent-props': ['off'], // Clashes with ESLint `indent` rule
 		'react/jsx-one-expression-per-line': ['off'],
 		'react/jsx-props-no-spreading': ['off'],
 		'react/no-multi-comp': ['off'],
-		'react/jsx-filename-extension': ['error', {extensions: ['.jsx', '.test.js']}],
+		'react/prop-types': ['off'],
 		'react/react-in-jsx-scope': ['off'],
 		'no-underscore-dangle': ['off']
 	}
